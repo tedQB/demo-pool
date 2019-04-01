e01f774ac48d014038e85fea0539fe3e6e10ffd0
kapaidashu <kasia0001@126.com>
2019-04-01 22:34:31 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+  This CodeSandbox has been automatically generated using
+  `codesandboxer`. If you're curious how that happened, you can
+  check out our docs here: https://github.com/noviny/codesandboxer
+
+  If you experience any struggles with this sandbox, please raise an issue
+  on github. :)
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './example';
+
+ReactDOM.render(
+<App />,
+document.getElementById('root')
+);
